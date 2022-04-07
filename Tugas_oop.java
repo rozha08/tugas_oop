9f43d4defbf92fab5ddcf8225c0dd224dee1c630
rozha08 <103170109+rozha08@users.noreply.github.com>
2022-04-07 13:24:56 +0700

    tugas oop

---
+++ Tugas_oop.java
@@ -0,0 +1,19 @@
+
+package tugas_oop;
+class Motor {
+String warna;
+String model;
+}
+public class Tugas_oop {
+
+    
+    public static void main(String[] args) {
+       Motor nama1 = new Motor();
+       nama1.warna = "biru";
+       nama1.model = "vixion";
+
+        System.out.println("warna : " + nama1.warna);
+        System.out.println("model : " + nama1.model);// TODO code application logic here
+    }
+    
+}
